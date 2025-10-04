a1066f30dc32ec2c20e0b57781cfa68c1dbca2ff
Aditya2376 <109151812+Aditya2376@users.noreply.github.com>
2025-10-04 13:01:26 +0530

    chore: consolidate all recent feature changes

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,11 +1,24 @@
-$(document).ready(function() {
-    $('.work-experience-accordion-button').on('click', function() {
+$(document).ready(function () {
+    $('.work-experience-accordion-button').on('click', function () {
         // The accordion content is the next element sibling
         var content = $(this).next('.work-experience-box-content');
 
         // Close all other accordions
         $('.work-experience-box-content').not(content).slideUp();
         $('.work-experience-accordion-button').not(this).removeClass('active');
+
+        // Toggle the clicked accordion
+        content.slideToggle();
+        $(this).toggleClass('active');
+    });
+
+    $('.custom-accordion-button').on('click', function () {
+        // The accordion content is the next element sibling
+        var content = $(this).next('.custom-accordion-content');
+
+        // Close all other accordions
+        $('.custom-accordion-content').not(content).slideUp();
+        $('.custom-accordion-button').not(this).removeClass('active');
 
         // Toggle the clicked accordion
         content.slideToggle();
